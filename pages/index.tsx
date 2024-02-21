7ea83dfe239f76aa24dfc3be243e0765d2a1e911
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2024-02-20 21:30:50 -0500

    flicker title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
-import styles from '@/styles/Home.module.css';
+import styles from '@/styles/App.module.css';
 import Header from '@/components/Header';
-
+import FlickerTitle from '@/components/FlickerTitle';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
@@ -12,7 +12,7 @@ export default function Home() {
 			<Header />
 			<main className={`${styles.main} ${inter.className}`}>
 				<h1>FREDERIC CARTIER</h1>
-				<p>COMING SOON</p>
+				<FlickerTitle title="coming soon" />
 			</main>
 		</>
 	);
