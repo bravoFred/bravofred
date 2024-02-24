d707d8d7f6f003898be9834234890100bb4d0126
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2024-02-24 11:49:24 -0500

    better

--- components/Portals.tsx
+++ components/Portals.tsx
@@ -10,50 +10,50 @@ import {
 } from '@react-three/drei';
 import { useControls } from 'leva';
 
-function Side({ rotation = [0, 0, 0], bg = '#f0f0f0', children, index }) {
-	const mesh = useRef<THREE.Mesh>(null!);
-	const { worldUnits } = useControls({ worldUnits: false });
-	const { nodes } = useGLTF('/models/aobox-transformed.glb');
-	useFrame((state, delta) => {
-		mesh.current.rotation.x = mesh.current.rotation.y += delta;
-	});
-	return (
-		<MeshPortalMaterial worldUnits={worldUnits} attach={`material-${index}`}>
-			{/** Everything in here is inside the portal and isolated from the canvas */}
-			<ambientLight intensity={0.5} />
-			<Environment preset="city" />
-			{/** A box with baked AO */}
-			<mesh castShadow receiveShadow rotation={rotation} geometry={nodes.Cube.geometry}>
-				<meshStandardMaterial
-					aoMapIntensity={1}
-					aoMap={nodes.Cube.material.aoMap}
-					color={bg}
-				/>
-				<spotLight
-					castShadow
-					color={bg}
-					intensity={2}
-					position={[10, 10, 10]}
-					angle={0.15}
-					penumbra={1}
-					shadow-normalBias={0.05}
-					shadow-bias={0.0001}
-				/>
-			</mesh>
-			{/** The shape */}
-			<mesh castShadow receiveShadow ref={mesh}>
-				{children}
-				<meshLambertMaterial color={bg} />
-			</mesh>
-		</MeshPortalMaterial>
-	);
-}
+// function Side({ rotation = [0, 0, 0], bg = '#f0f0f0', children, index: index = 0 }) {
+// 	const mesh = useRef<THREE.Mesh>(null!);
+// 	const { worldUnits } = useControls({ worldUnits: false });
+// 	const { nodes } = useGLTF('/models/aobox-transformed.glb');
+// 	useFrame((state, delta) => {
+// 		mesh.current.rotation.x = mesh.current.rotation.y += delta;
+// 	});
+// 	return (
+// 		<MeshPortalMaterial worldUnits={worldUnits} attach={`material-${index}`}>
+// 			{/** Everything in here is inside the portal and isolated from the canvas */}
+// 			<ambientLight intensity={0.5} />
+// 			<Environment preset="city" />
+// 			{/** A box with baked AO */}
+// 			<mesh castShadow receiveShadow rotation={rotation} geometry={nodes.Cube.geometry}>
+// 				<meshStandardMaterial
+// 					aoMapIntensity={1}
+// 					aoMap={nodes.Cube.material.aoMap}
+// 					color={bg}
+// 				/>
+// 				<spotLight
+// 					castShadow
+// 					color={bg}
+// 					intensity={2}
+// 					position={[10, 10, 10]}
+// 					angle={0.15}
+// 					penumbra={1}
+// 					shadow-normalBias={0.05}
+// 					shadow-bias={0.0001}
+// 				/>
+// 			</mesh>
+// 			{/** The shape */}
+// 			<mesh castShadow receiveShadow ref={mesh}>
+// 				{children}
+// 				<meshLambertMaterial color={bg} />
+// 			</mesh>
+// 		</MeshPortalMaterial>
+// 	);
+// }
 export default function Portals() {
 	return (
 		<mesh castShadow receiveShadow>
 			<boxGeometry args={[2, 2, 2]} />
 			<Edges />
-			<Side rotation={[0, 0, 0]} bg="orange" index={0}>
+			{/* <Side rotation={[0, 0, 0]} bg="orange" index={0}>
 				<torusGeometry args={[0.65, 0.3, 64]} />
 			</Side>
 			<Side rotation={[0, Math.PI, 0]} bg="lightblue" index={1}>
@@ -70,7 +70,7 @@ export default function Portals() {
 			</Side>
 			<Side rotation={[0, Math.PI / 2, 0]} bg="hotpink" index={5}>
 				<dodecahedronGeometry />
-			</Side>
+			</Side> */}
 		</mesh>
 	);
 }
