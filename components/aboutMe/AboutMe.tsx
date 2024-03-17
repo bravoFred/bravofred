17b1243547bc80e4f073ed3d9a348b01303062de
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2024-03-17 18:13:22 -0400

    Update AboutMe.tsx

--- components/aboutMe/AboutMe.tsx
+++ components/aboutMe/AboutMe.tsx
@@ -34,12 +34,14 @@ export default function AboutMe(props) {
 	const icon2Ref = useRef<THREE.Group>();
 
 	const baseSpeed = 0.1;
-	const scaleBase = mobile ? 0.66 : 0.66;
-	const scaleActive = mobile ? 0.75 : 0.8;
+	const scaleBase = mobile ? 0.5 : 0.66;
+	const scaleActive = mobile ? 0.65 : 0.8;
 	const scaleUpFactor = 0.25;
 	const scaleDownFactor = 0.25;
 	const scaleUpSpeed = 0.1;
 	const scaleDownSpeed = 0.1;
+	const iconX = mobile ? -0.33 : 0.7;
+	const iconY = mobile ? 0.8 : 0.8;
 	const [text, setText] = useState({
 		ref: useRef<THREE.Group>(),
 		active: mobile ? new THREE.Vector3(0, 1, 0) : new THREE.Vector3(-0.5, 1, 0.2),
@@ -49,11 +51,10 @@ export default function AboutMe(props) {
 		enterSpeed: baseSpeed * 1.5,
 		exitSpeed: baseSpeed / 4,
 	});
-	const iconX = mobile ? 0.65 : 0.7;
-	const iconY = mobile ? 0.8 : 0.8;
+
 	const [icon1, setIcon1] = useState({
 		ref: useRef<THREE.Group>(),
-		active: mobile ? new THREE.Vector3(-0.33, 0.9, 0) : new THREE.Vector3(iconX, 0.85, 0),
+		active: mobile ? new THREE.Vector3(iconX, 1.1, 0) : new THREE.Vector3(iconX, 0.85, 0),
 		hidden: new THREE.Vector3(0, 0, 20),
 		scaleHovered: new THREE.Vector3(scaleActive, scaleActive, scaleActive),
 		hiddenScale: new THREE.Vector3(scaleBase, scaleBase, scaleBase),
@@ -62,7 +63,7 @@ export default function AboutMe(props) {
 	});
 	const [icon2, setIcon2] = useState({
 		ref: useRef<THREE.Group>(),
-		active: mobile ? new THREE.Vector3(0.33, 0.15, 0) : new THREE.Vector3(iconX + 0.4, 0.45, 0),
+		active: mobile ? new THREE.Vector3(0.33, 0.2, 0) : new THREE.Vector3(iconX + 0.4, 0.45, 0),
 		hidden: new THREE.Vector3(0, 0, 25),
 		scaleHovered: new THREE.Vector3(scaleActive, scaleActive, scaleActive),
 		hiddenScale: new THREE.Vector3(scaleBase, scaleBase, scaleBase),
@@ -71,7 +72,7 @@ export default function AboutMe(props) {
 	});
 	const [icon3, setIcon3] = useState({
 		ref: useRef<THREE.Group>(),
-		active: mobile ? new THREE.Vector3(-0.33, -0.15, 0) : new THREE.Vector3(iconX, 0.05, 0),
+		active: mobile ? new THREE.Vector3(iconX, -0.1, 0) : new THREE.Vector3(iconX, 0.05, 0),
 		hidden: new THREE.Vector3(0, 0, 30),
 		scaleHovered: new THREE.Vector3(scaleActive, scaleActive, scaleActive),
 		hiddenScale: new THREE.Vector3(scaleBase, scaleBase, scaleBase),
