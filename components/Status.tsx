47c8e8fcb8c8a31a76fd0f2c0e779f55741d3283
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2024-03-17 19:04:35 -0400

    .

--- components/Status.tsx
+++ components/Status.tsx
@@ -1,47 +1,19 @@
 'use client';
 import styles from './Status.module.scss';
 import { useEffect, useState } from 'react';
+import { getCurrentHour, getCurrentMinute, getAmPm, getWeekDay, getCurrentSecond } from './Time';
+
 export default function Status() {
-	// get current time
-	const date = new Date();
-	const hours = date.getHours();
-	const minutes = date.getMinutes();
-	const seconds = date.getSeconds();
-	const [time, setTime] = useState({
-		hours: hours,
-		minutes: minutes,
-		seconds: seconds,
-	});
-	// get am or pm
-	const amPm = hours >= 12 ? 'pm' : 'am';
-	// update time every second
-	// setTimeout(() => {
-	// 	const date = new Date();
-	// 	const hours = date.getHours();
-	// 	const minutes = date.getMinutes();
-	// 	const seconds = date.getSeconds();
-	// 	setTime({
-	// 		hours: hours,
-	// 		minutes: minutes,
-	// 		seconds: seconds,
-	// 	});
-	// }, 1000);
-	// day of the week
-	const day = date.getDay();
-	const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-	const dayOfWeek = days[day];
+	const [userTime, setUserTime] = useState(``);
+	function updateTimeString() {
+		setUserTime(
+			`${getWeekDay()}, ${getCurrentHour()}:${getCurrentMinute()}:${getCurrentSecond()} ${getAmPm()}`
+		);
+	}
 	useEffect(() => {
-		// interval to update time every second
+		updateTimeString();
 		const interval = setInterval(() => {
-			const date = new Date();
-			const hours = date.getHours();
-			const minutes = date.getMinutes();
-			const seconds = date.getSeconds();
-			setTime({
-				hours: hours,
-				minutes: minutes,
-				seconds: seconds,
-			});
+			updateTimeString();
 		}, 1000);
 		return () => clearInterval(interval);
 	}, []);
@@ -49,10 +21,7 @@ export default function Status() {
 	return (
 		<div className={styles.status}>
 			<p className={styles.status_text}>Atlanta, Georgia</p>
-			<p className={styles.status_text}>
-				{dayOfWeek}, {hours > 12 ? hours - 12 : hours}:{time.minutes}:
-				{time.seconds < 10 ? '0' + time.seconds : time.seconds} {amPm}
-			</p>
+			<p className={styles.status_text}>{userTime}</p>
 		</div>
 	);
 }
