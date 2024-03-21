6316454cc998912a561bf10b54dff996dfae11e0
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2024-03-20 20:08:40 -0400

    .

--- components/Credits.tsx
+++ components/Credits.tsx
@@ -1,30 +1,33 @@
 import styles from './Credits.module.scss';
 import Link from 'next/link';
+import InputContextProvider from '@/store/inputContext';
+import { useContext } from 'react';
 export default function Credits() {
-	const designer = `Frederic Cartier`;
+	const designer = ` Frederic Cartier`;
 	const developer = `Thomas Matlock`;
+	const { aboutMeActive } = useContext(InputContextProvider);
 	const currentYear = new Date().getFullYear();
 	return (
-		<div className={styles.credits}>
+		<div className={styles.credits} style={aboutMeActive ? { opacity: '1' } : { opacity: '0' }}>
 			<div className={styles.credit}>
 				<p className={styles.credit_text}>
 					&copy; {designer} {currentYear}. All rights reserved.
 				</p>
 			</div>
 			<div className={styles.credit}>
-				<p className={styles.credit_text}>Designed by</p>
-				<p className={styles.credit_text_bold}>{designer}</p>{' '}
-				<p className={styles.credit_text}>Developed by</p>
-				<Link href="https://www.thomasmatlock.com" target="_blank">
-					<p className={styles.credit_text_bold} style={{ cursor: 'pointer' }}>
-						{developer}
-					</p>
-				</Link>
+				<div className={styles.credit_item}>
+					<p className={styles.credit_text}>Design</p>
+					<p className={styles.credit_text_bold}>{designer}</p>
+				</div>
+				<div className={styles.credit_item}>
+					<p className={styles.credit_text}>Development</p>
+					<Link href="https://www.thomasmatlock.com" target="_blank">
+						<p className={styles.credit_text_bold} style={{ cursor: 'pointer' }}>
+							{developer}
+						</p>
+					</Link>
+				</div>
 			</div>
-			{/* <p className={styles.credit_text}>Designed by</p>
-			<p className={styles.credit_person}>{designer}</p>
-			<p className={styles.credit_text}>DEVELOPED BY</p>
-			<p className={styles.credit_person}>{developer}</p> */}
 		</div>
 	);
 }
